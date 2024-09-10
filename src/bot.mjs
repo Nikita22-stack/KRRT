ca0d816c25b21acb32f418a8099088fbac9d1041
Waik <safmp333@list.ru>
2024-09-11 00:27:51 +0300

    Standart

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,27 +1,10 @@
-import TeleBot from "telebot";
+import TeleBot from "telebot"
 
-const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
+const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
-bot.on(['/rep'], (msg) => {
-    const chatId = msg.chat.id; // Используем идентификатор чата
-    const username = msg.from.first_name;
-
-    const keyboard = {
-        inline_keyboard: [
-            [
-                {
-                    text: 'Открыть приложение',
-                    web_app: { url: 'https://watertouch.vercel.app/' }
-                }
-            ]
-        ]
-    };
-
-    return bot.sendMessage(
-        chatId,
-        `Привет, ${username}! Прикоснись к воде. 💦`,
-        { replyMarkup: keyboard } // Отправляем клавиатуру
-    );
+bot.on('/rep', (msg) => {
+    const chatId = msg.chat.id; // Идентификатор чата, из которого пришло сообщение
+    bot.sendMessage(chatId, 'Привет!');
 });
 
-export default bot;
+bot.start();
