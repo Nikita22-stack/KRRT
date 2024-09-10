c63c898af31e9b36a1c68e2b3382e94fd54adf9f
Waik <safmp333@list.ru>
2024-09-11 01:56:21 +0300

    Standart

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,13 +1,29 @@
 import TeleBot from "telebot"
+import { database, ref, set, update } from './config';
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
 bot.on(['/rep'], (msg) => {
     const chatId = msg.chat.id;
-    const username = msg.from.first_name;
 
-    // Отправляем сообщение с клавиатурой и никнеймом пользователя
-    return bot.sendMessage(chatId, `Привет, ${username}!`);
+    // Создаем ссылку на всех пользователей в базе данных Firebase
+    const userRef = ref(database, `users`);
+
+    // Получаем всех пользователей и собираем их репутацию
+    onValue(userRef, (snapshot) => {
+        const users = snapshot.val();
+        let response = 'Репутация пользователей:\n';
+
+        // Проходим по всем пользователям и собираем информацию о репутации
+        for (const key in users) {
+            if (users[key].rep !== undefined) {
+                response += `Пользователь: ${key}, Репутация: ${users[key].rep}\n`;
+            }
+        }
+
+        // Отправляем сообщение с количеством репутаций
+        return bot.sendMessage(chatId, response);
+    });
 });
 
 export default bot
