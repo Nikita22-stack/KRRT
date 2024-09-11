c3dfbf4e1449ad66f76f3e71d6866aa4421d4861
Waik <safmp333@list.ru>
2024-09-11 03:36:22 +0300

    Standart

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,23 +1,16 @@
-import TeleBot from "telebot";
-import { database, ref, get } from "./config";
+import TeleBot from 'telebot';
+import { database, ref, get } from './config';
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
 
 bot.on('/rep', (msg) => {
     const chatId = msg.chat.id;
-    const usersRef = ref(database, 'user'); // путь к узлу 'user'
+    const userRef = ref(database, 'user');
 
-    get(usersRef).then((snapshot) => {
+    get(userRef).then((snapshot) => {
         if (snapshot.exists()) {
             const data = snapshot.val();
             let repMessage = 'Количество rep:\n';
-
-            // Проходим по всем пользователям и добавляем их значения rep в сообщение
-            for (const userId in data) {
-                if (data[userId].rep) {
-                    repMessage += `${userId}: ${data[userId].rep}\n`;
-                }
-            }
 
             return bot.sendMessage(chatId, repMessage);
         }
