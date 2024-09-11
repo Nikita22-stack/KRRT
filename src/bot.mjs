5a23dbbc0e70d651f7344940344655588f6b0f3f
Waik <safmp333@list.ru>
2024-09-11 03:04:29 +0300

    Standart

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,12 +1,26 @@
-import TeleBot from "telebot"
-import { database, ref, get } from './config';
+import TeleBot from 'telebot';
+import { database, ref, get } from './config'; // убедитесь, что вы правильно настроили Firebase
 
-const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
+const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
 
-bot.on(['/rep'], (msg) => {
+bot.on('/rep', async (msg) => {
     const chatId = msg.chat.id;
+    const usersRef = ref(database, 'user'); // путь к узлу 'user'
 
-    return bot.sendMessage(chatId, `фф`);
+    const snapshot = await get(usersRef);
+    if (snapshot.exists()) {
+        const data = snapshot.val();
+        let repMessage = 'Количество rep для каждого пользователя:\n';
+
+        // Проходим по всем пользователям и добавляем их значения rep в сообщение
+        for (const userId in data) {
+            if (data[userId].rep) {
+                repMessage += `${userId}: ${data[userId].rep}\n`;
+            }
+        }
+
+        return bot.sendMessage(chatId, repMessage);
+    }
 });
 
-export default bot
+export default bot;
