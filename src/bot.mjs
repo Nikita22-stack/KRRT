ae98a1fb8dc3500f5732440515769cc1f794647a
Waik <hmelevnikita8@gmail.com>
2024-10-23 03:35:01 +0300

    first commit

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,6 +1,6 @@
 import TeleBot from 'telebot';
 
-const bot = new TeleBot('7091205404:AAEOaq8XrCWe3AlNFN4T2kEweeQoX_6h0j4'); // Замените на токен вашего бота
+const bot = new TeleBot('7343134073:AAGDOTaMuL9nBkJsKukiKcRTfbt2_2joa_g'); // Замените на токен вашего бота
 
 let lastMessages = [];
 let lastPayments = [];
@@ -10,12 +10,12 @@ bot.on('text', async (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
 
-    if (text === '/startwork') {
+    if (text === '/更新') {
         sum = 0; exchangeRate = 0; bought = 0;
-        await bot.sendMessage(chatId, `Total Cash: ${sum} RUB | 0 USDT\nSend is back: ${bought} USDT\nExchange rate: ${exchangeRate} RUB`);
-    } else if (text.startsWith('/Exchangerate ')) {
+        await bot.sendMessage(chatId, `总计: ${sum} RUB | 0 USDT\n发送返回: ${bought} USDT\n汇率: ${exchangeRate} RUB`);
+    } else if (text.startsWith('/交换')) {
         exchangeRate = parseFloat(text.split(' ')[1]);
-        await bot.sendMessage(chatId, `Total Cash: ${sum} RUB | ${(sum / exchangeRate).toFixed(2)} USDT\nSend is back: ${bought} USDT\nExchange rate: ${exchangeRate} RUB`);
+        await bot.sendMessage(chatId, `总计: ${sum} RUB | ${(sum / exchangeRate).toFixed(2)} USDT\n发送返回: ${bought} USDT\n汇率: ${exchangeRate} RUB`);
     } else if (text.startsWith('+')) {
         const amount = parseFloat(text.substring(1));
         lastMessages.push(`${new Date().toLocaleTimeString()} ${amount}`);
@@ -32,15 +32,15 @@ bot.on('text', async (msg) => {
 
         await bot.sendMessage(chatId, `${String.fromCharCode(0x2705)} UPDATE ${String.fromCharCode(0x2705)}\n` +
             `------------------------------\n` +
-            `Total Cash: ${sum.toFixed(2)} RUB | ${(sum / exchangeRate).toFixed(2)} USDT\n` +
-            `Send is back: ${bought} USDT\n` +
-            `Exchange rate: ${exchangeRate} RUB\n` +
+            `总计: ${sum.toFixed(2)} RUB | ${(sum / exchangeRate).toFixed(2)} USDT\n` +
+            `发送返回: ${bought} USDT\n` +
+            `汇率: ${exchangeRate} RUB\n` +
             `------------------------------\n` +
-            `Last checks (5):\n${lastMessagesText}\n` +
+            `最近5笔交易:\n${lastMessagesText}\n` +
             `------------------------------\n` +
-            `Last Payments:\n${lastPaymentsText}\n` +
+            `最后一次付款:\n${lastPaymentsText}\n` +
             `------------------------------\n` +
-            `Total checks number: ${checksNum}\n`);
+            `支票总数: ${checksNum}\n`);
     } else if (text.startsWith('-')) {
         const amount = parseFloat(text.substring(1));
         lastMessages.push(`${new Date().toLocaleTimeString()} -${amount}`);
@@ -57,16 +57,16 @@ bot.on('text', async (msg) => {
 
         await bot.sendMessage(chatId, `${String.fromCharCode(0x2705)} UPDATE ${String.fromCharCode(0x2705)}\n` +
             `------------------------------\n` +
-            `Total Cash: ${sum.toFixed(2)} RUB | ${(sum / exchangeRate).toFixed(2)} USDT\n` +
-            `Send is back: ${bought} USDT\n` +
-            `Exchange rate: ${exchangeRate} RUB\n` +
+            `总计: ${sum.toFixed(2)} RUB | ${(sum / exchangeRate).toFixed(2)} USDT\n` +
+            `发送返回: ${bought} USDT\n` +
+            `汇率: ${exchangeRate} RUB\n` +
             `------------------------------\n` +
-            `Last checks (5):\n${lastMessagesText}\n` +
+            `最近5笔交易:\n${lastMessagesText}\n` +
             `------------------------------\n` +
-            `Last Payments:\n${lastPaymentsText}\n` +
+            `最后一次付款:\n${lastPaymentsText}\n` +
             `------------------------------\n` +
-            `Total checks number: ${checksNum}\n`);
-    } else if (text.startsWith('/Payments ')) {
+            `支票总数: ${checksNum}\n`);
+    } else if (text.startsWith('/付款')) {
         const payment = parseFloat(text.split(' ')[1]);
         bought += payment;
         lastPayments.push(`${new Date().toLocaleTimeString()} ${payment} USDT`);
@@ -76,15 +76,15 @@ bot.on('text', async (msg) => {
 
         await bot.sendMessage(chatId, `${String.fromCharCode(0x2705)} UPDATE ${String.fromCharCode(0x2705)}\n` +
             `------------------------------\n` +
-            `Total Cash: ${sum.toFixed(2)} RUB | ${(sum / exchangeRate).toFixed(2)} USDT\n` +
-            `Send is back: ${bought.toFixed(2)} USDT\n` +
-            `Exchange rate: ${exchangeRate} RUB\n` +
+            `总计: ${sum.toFixed(2)} RUB | ${(sum / exchangeRate).toFixed(2)} USDT\n` +
+            `发送返回: ${bought.toFixed(2)} USDT\n` +
+            `汇率: ${exchangeRate} RUB\n` +
             `------------------------------\n` +
-            `Last checks (5):\n${lastMessagesText}\n` +
+            `最近5笔交易:\n${lastMessagesText}\n` +
             `------------------------------\n` +
-            `Last Payments:\n${lastPaymentsText}\n` +
+            `最后一次付款:\n${lastPaymentsText}\n` +
             `------------------------------\n` +
-            `Total checks number: ${checksNum}\n`);
+            `支票总数: ${checksNum}\n`);
     }
 });
 
